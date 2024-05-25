0dafefe071ea7d6e2641c2d6123cf6d00bcc2800
Kevin Grant <anmoron11@gmail.com>
2024-05-25 14:14:28 -0400

    Implemented feature based on assigment requests.

--- src/features/photos/photos.slice.js
+++ src/features/photos/photos.slice.js
@@ -1,22 +1,38 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { selectSearchTerm } from '../search/search.slice';
-import photos from './photos.data.js';
+import { createSlice } from "@reduxjs/toolkit";
+import { selectSearchTerm } from "../search/search.slice";
+import photos from "./photos.data.js";
 
 const initialState = {
   photos,
 };
 
 const options = {
-  name: 'photos',
+  name: "photos",
   initialState,
   reducers: {
-    // Task 1: Create an `addPhoto()` case reducer that adds a photo to state.photos. 
+    // Task 1: Create an `addPhoto()` case reducer that adds a photo to state.photos.
     // Task 1 Hint: You can use state.photos.unshift()
     // `unshift()` documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift
-   
+    addPhoto: (state, action) => {
+      const { caption, imageUrl } = action.payload;
+
+      state.photos.unshift({
+        id: state.photos.length + 1,
+        caption,
+        imageUrl,
+      });
+    },
     // Task 6: Create an `removePhoto()` case reducer that removes a photo from state.photos
     // Task 6 Hint: You can use state.photos.splice()
     // `splice()` documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+
+    removePhoto: (state, action) => {
+      console.log(action.payload);
+      state.photos.splice(
+        state.photos.findIndex((photo) => photo.id === action.payload),
+        1
+      );
+    },
   },
 };
 
@@ -29,4 +45,8 @@ export default photosSlice.reducer;
 export const selectAllPhotos = (state) => state.photos.photos;
 export const selectFilteredPhotos = (state) => {
   // Task 12: Complete `selectFilteredPhotos()` selector to return a filtered list of photos whose captions match the user's search term
+  const _photos = selectAllPhotos(state);
+  const searchTerm = selectSearchTerm(state);
+
+  return _photos.filter(photo => photo.caption.toLowerCase().includes(searchTerm.toLowerCase()));
 };
